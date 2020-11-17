553bf7c3490447378cb354da37aebe1a204a70a7
jwon42 <jwon@student.42seoul.kr>
2020-11-17 17:38:00 +0900

    UPDATE: Header indentation

--- 00/ex01/contact.hpp
+++ 00/ex01/contact.hpp
@@ -7,14 +7,14 @@ class Contact
 {
 	public:
 	Contact();
-	int		input_infomation(int idx);
-	void	view_contact_list();
-	void	view_contact_detail(int	idx);
+		int		input_infomation(int idx);
+		void	view_contact_list();
+		void	view_contact_detail(int	idx);
 
 	private:
-	int				_idx;
-	std::string		_contact_infomation[11];
-	std::string		_contact_fields[11];
+		int				_idx;
+		std::string		_contact_infomation[11];
+		std::string		_contact_fields[11];
 };
 
 #endif
--- 00/ex01/phonebook.hpp
+++ 00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: jwon <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:20:04 by jwon              #+#    #+#             */
-/*   Updated: 2020/11/16 14:21:53 by jwon             ###   ########.fr       */
+/*   Updated: 2020/11/17 17:36:38 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ class Phonebook
 {
 	public:
 	Phonebook();
-	void	ascii_art();
-	int		add_contact();
-	int		select_contact();
-	int		view_phonebook();
+		void	ascii_art();
+		int		add_contact();
+		int		select_contact();
+		int		view_phonebook();
 
 	private:
-	Contact	_contacts[8];
-	int		_number_of_contacts;
+		Contact	_contacts[8];
+		int		_number_of_contacts;
 };
 
 #endif
