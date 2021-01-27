76671d0547c44000530f9bbb010b1e11dc39cfd8
jwon42 <jwon@student.42seoul.kr>
2021-01-28 00:27:38 +0900

    UPDATE: CPP_Module_04

--- 04/ex00/Sorcerer.hpp
+++ 04/ex00/Sorcerer.hpp
@@ -6,7 +6,7 @@
 /*   By: jwon <jwon@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 17:17:43 by jwon              #+#    #+#             */
-/*   Updated: 2021/01/26 17:52:07 by jwon             ###   ########.fr       */
+/*   Updated: 2021/01/28 00:27:12 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 class Sorcerer
 {
 	public:
-		// Canonical Form // Default constructor (기본생성자)
+		// Canonical Form
 		Sorcerer(std::string const name, std::string const title); // Constructor Overloading (생성자 오버로딩)
 		Sorcerer(Sorcerer const &ref); // Copy constructor (복사생성자)
 		Sorcerer& operator=(Sorcerer const &ref); // Assignation operator (할당연산자)
