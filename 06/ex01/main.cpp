0b5657acf5e357f62a56c2bdeadf94c5b4e72f9f
jwon42 <jwon@student.42seoul.kr>
2021-01-29 13:26:13 +0900

    UPDATE: CPP_Module_06/ex01/main.cpp

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jwon <jwon@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 02:24:41 by jwon              #+#    #+#             */
-/*   Updated: 2021/01/29 11:44:15 by jwon             ###   ########.fr       */
+/*   Updated: 2021/01/29 13:25:57 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,3 @@ int main(void)
 	delete data;
 	return (EXIT_SUCCESS);
 }
-
-// int main(void)
-// {
-// 	void *raw = serialize();
-// 	Data *data = deserialize(raw);
-
-// 	std::cout << data->s1 << " " << data->n << " " << data->s2 << std::endl;
-
-// 	delete static_cast<char*>(raw);
-// 	delete data;
-
-// 	return (0);
-// }
